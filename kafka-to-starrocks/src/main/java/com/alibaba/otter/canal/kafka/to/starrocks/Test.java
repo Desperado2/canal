f935cea14ac43aa7eb0a0770b536c825b50ff86e
mujingjing <mujingjing@315app.net>
2024-02-01 18:25:40 +0800

    新增质量文件

---
+++ kafka-to-starrocks/src/main/java/com/alibaba/otter/canal/kafka/to/starrocks/Test.java
@@ -0,0 +1,16 @@
+package com.alibaba.otter.canal.kafka.to.starrocks;
+
+/**
+ * 1
+ *
+ * @author mujingjing
+ * @date 2024/2/1
+ **/
+
+/**
+ *
+ * @author mujingjing
+ * @date 2024/2/1
+ **/
+public class Test {
+}
