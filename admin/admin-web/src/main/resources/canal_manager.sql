fe3c778dc33a85482b31e9eec03efdb38bde4daa
mujingjing <mujingjing@315app.net>
2024-02-26 18:18:16 +0800

    修改SQL脚本

--- admin/admin-web/src/main/resources/canal_manager.sql
+++ admin/admin-web/src/main/resources/canal_manager.sql
@@ -98,6 +98,71 @@ CREATE TABLE `canal_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Table structure for canal_table_structure_mapping_env
+-- ----------------------------
+DROP TABLE IF EXISTS `canal_table_structure_mapping_env`;
+CREATE TABLE `canal_table_structure_mapping_env` (
+ `id` bigint NOT NULL AUTO_INCREMENT,
+ `env_code` varchar(63) DEFAULT NULL COMMENT '环境编码',
+ `env_name` varchar(200) NOT NULL COMMENT '环境名称',
+ `description` text COMMENT '描述信息',
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `env_code` (`env_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for canal_table_structure_mapping
+-- ----------------------------
+DROP TABLE IF EXISTS `canal_table_structure_mapping`;
+CREATE TABLE `canal_table_structure_mapping` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `env_code` varchar(63) DEFAULT NULL COMMENT '环境编码',
+    `src_database` varchar(200) NOT NULL COMMENT '源库名称',
+    `src_table` varchar(200) NOT NULL COMMENT '原表名称',
+    `dst_database` varchar(200) NOT NULL COMMENT '目标库名称',
+    `dst_table` varchar(200) NOT NULL COMMENT '目标表名称',
+    `content` text NOT NULL COMMENT '配置文件内容',
+    `content_md5` varchar(128) DEFAULT NULL COMMENT '配置文件内容md5',
+    `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for canal_consumer_instance_config
+-- ----------------------------
+DROP TABLE IF EXISTS `canal_consumer_instance_config`;
+CREATE TABLE `canal_consumer_instance_config` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `task_id` varchar(255)  DEFAULT NULL,
+  `cluster_id` bigint DEFAULT NULL COMMENT '集群id',
+  `task_node_id` bigint DEFAULT NULL COMMENT 'task node Id',
+  `name` varchar(45)  NOT NULL COMMENT '名称',
+  `status` varchar(45)  DEFAULT NULL COMMENT '状态',
+  `content` text NOT NULL COMMENT '配置内容',
+  `group_id` varchar(100) DEFAULT NULL COMMENT '消费组id',
+  `content_md5` varchar(128) DEFAULT NULL COMMENT '内容md5',
+  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '变更时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `name_UNIQUE` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for canal_consumer_instance_config
+-- ----------------------------
+DROP TABLE IF EXISTS `canal_task_node_server`;
+CREATE TABLE `canal_task_node_server` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cluster_id` bigint DEFAULT NULL,
+  `name` varchar(63) NOT NULL,
+  `ip` varchar(63) NOT NULL,
+  `port` int DEFAULT NULL,
+  `status` varchar(45)  DEFAULT NULL,
+  `modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
 SET FOREIGN_KEY_CHECKS = 1;
 
 -- ----------------------------
